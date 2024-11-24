90057a9eb815b6dcdf298ec544305e5dca54fd7b
Rohan Maharjan <rohan.maharjan@harness.io>
2024-11-24 19:11:50 +0545

    github-actopms

--- script.sh
+++ script.sh
@@ -5,9 +5,17 @@
      -not -name "CONTRIBUTING.md" \
      -exec basename {} \; | grep -vE '^[a-z0-9]+(-[a-z0-9]+)*\.md$')
    
-   if [ -n "$invalid_files" ]; then
+   if [ -n "$(find . -type f -name '*.md' \
+     -not -path "./node_modules/*" \
+     -not -name "README.md" \
+     -not -name "CONTRIBUTING.md" \
+     -exec basename {} \; | grep -vE '^[a-z0-9]+(-[a-z0-9]+)*\.md$')" ]; then
        echo "The following Markdown filenames are not in kebab case:"
-       echo "$invalid_files"
+       echo "$(find . -type f -name '*.md' \
+     -not -path "./node_modules/*" \
+     -not -name "README.md" \
+     -not -name "CONTRIBUTING.md" \
+     -exec basename {} \; | grep -vE '^[a-z0-9]+(-[a-z0-9]+)*\.md$')"
    
      
    else
