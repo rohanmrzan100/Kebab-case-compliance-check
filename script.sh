4724634d1dc948030b5dcacbfb82925398b84036
Rohan Maharjan <rohan.maharjan@harness.io>
2024-11-24 17:56:54 +0545

    github-actopms

---
+++ script.sh
@@ -0,0 +1,16 @@
+  # Find all .md files excluding node_modules, README.md, and CONTRIBUTING.md, and check filenames for kebab case
+   invalid_files=$(find . -type f -name '*.md' \
+     -not -path "./node_modules/*" \
+     -not -name "README.md" \
+     -not -name "CONTRIBUTING.md" \
+     -exec basename {} \; | grep -vE '^[a-z0-9]+(-[a-z0-9]+)*\.md$')
+   
+   if [ -n "$invalid_files" ]; then
+       echo "The following Markdown filenames are not in kebab case:"
+       echo "$invalid_files"
+       exit 1
+     
+   else
+       echo "All Markdown filenames are in kebab case."
+       exit 0
+   fi
